5019f355be8d239a699e4181183c95eeac8251f4
TanVek <marycourt111@gmail.com>
2026-09-08 19:23:56 -0400

    changes

--- testing/src/IntCollection.java
+++ testing/src/IntCollection.java
@@ -13,6 +13,9 @@ public class IntCollection {
    private int indexOf(int key) {
       // indexOf() implemented as a linear search.
       // YOUR CODE HERE
+      for(int i = 0; i < a.length; i++){
+         if( a[i] == key) return i;
+      }
       return -1;
    }
 }
